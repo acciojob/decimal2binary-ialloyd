1e325505243daf6582715effd041140f20592469
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-13 16:33:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function decimalToBinary(num) {
   //Write you code here
+	let res=""
+	while(num!=0){
+
+		res=res+num%2
+
+		num=Math.floor(num/2)
+
+		
+	}
+
+	return res
   
 }
+
+let num=prompt("Enter a number")
+
+alert(decimalToBinary(num))
 
 window.decimalToBinary = decimalToBinary;
